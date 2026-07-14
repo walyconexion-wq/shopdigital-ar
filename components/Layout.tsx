1bb2c95896fe94f907591f5d4f205aae1336b64f
Tu Nombre <tu@mail.com>
2026-07-14 20:00:22 -0300

    design: cambiar color de fondo en day mode a #1cc2ca

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -195,7 +195,7 @@ const Layout: React.FC<LayoutProps> = ({ allShops = [], globalConfig }) => {
             style={{ 
                 ...containerStyle, 
                 backgroundColor: shouldApplyDayMode 
-                    ? (isEnterprisePath ? '#f8fafc' : '#cda488') 
+                    ? (isEnterprisePath ? '#f8fafc' : '#1cc2ca') 
                     : bgColor 
             }}
         >
